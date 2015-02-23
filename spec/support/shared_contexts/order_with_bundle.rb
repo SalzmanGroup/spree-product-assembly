d2638befd58cfe7834ca5edd0ef1db5a6ce996f8
Kyle Macey <kyle@wholelattelove.com>
2015-02-23 13:23:56 -0500

    fix last of specs

--- spec/support/shared_contexts/order_with_bundle.rb
+++ spec/support/shared_contexts/order_with_bundle.rb
@@ -1,7 +1,7 @@
 shared_context "product is ordered as individual and within a bundle" do
-  let(:line_items) { build_stubbed_list(:line_item, 5) }
-  let(:order) { build_stubbed(:order) }
-  let(:parts) { build_stubbed_list(:variant, 3) }
+  let(:line_items) { create_list(:line_item, 5) }
+  let(:order) { create(:order) }
+  let(:parts) { create_list(:variant, 3) }
 
   let(:bundle_variant) { order.line_items.first.variant }
   let(:bundle) { bundle_variant.product }
@@ -9,10 +9,10 @@ shared_context "product is ordered as individual and within a bundle" do
   let(:common_product) { order.line_items.last.variant }
 
   before do
-    allow(order).to receive(:line_items).and_return(line_items)
+    order.line_items = line_items
     expect(bundle_variant).to_not be_nil
     expect(bundle_variant).to_not eql common_product
 
-    allow(bundle).to receive(:parts).and_return(parts + Array(common_product))
+    bundle.parts = parts + Array(common_product)
   end
 end
